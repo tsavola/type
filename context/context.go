aa38d23ba01e5abcc05cde40da23b877daaa6639
Timo Savola <timo.savola@iki.fi>
2024-10-02 22:39:52 +0300

    Package context

---
+++ context/context.go
@@ -0,0 +1,9 @@
+// Package context contains only a type alias for [context.Context].
+package context
+
+import (
+	"context"
+)
+
+// Context is a type alias for [context.Context].
+type Context = context.Context
